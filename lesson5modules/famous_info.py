bca8bf9711af96fb27a65b4c8d0f3d7deab9da08
akvc <activeru@gmail.com>
2022-06-27 22:04:32 +0300

    commit

--- lesson5modules/famous_info.py
+++ lesson5modules/famous_info.py
@@ -4,8 +4,12 @@
 # from tkinter import *
 
 # Импор всех функций из модуля
-from famous_persons import *
+#from famous_persons import *
+
+# Импорт нужных функций
+from famous_persons import get_random_person as gr
 
 def fp():
-    name, date = get_random_person()
+    #name, date = get_random_person()
+    name, date = gr()
     print(name, date)
--- new.py
+++ new.py
@@ -1,7 +1,34 @@
-#import lesson5modules\main.py
-from famous_info import fp
-#print('hello')
-#print('World')
+# import lesson5modules\main.py
+import sys
 
+sys.path.append('lesson5modules')
+from famous_info import fp
+import famous_info_copy
+
+# print('hello')
+# print('World')
+
+val = 0
 print('Go now')
-gr()
+while val == 0:
+    print('''1. создать папку;
+2. удалить (файл/папку);
+3. копировать (файл/папку);
+4. просмотр содержимого рабочей директории;
+5. посмотреть только папки;
+6. посмотреть только файлы;
+7. просмотр информации об операционной системе;
+8. создатель программы;
+9. играть в викторину;
+10. мой банковский счет;
+11. смена рабочей директории (*необязательный пункт);
+x. выход.''')
+    val = input('?')
+    print(val)
+    if (val=='1'):
+        print('Создаём папку')
+
+    if val != 'X' and val != 'x' and val != 'Ч' and val != 'ч':
+        val = 0
+    # fp()
+print (fp())
