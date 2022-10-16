d85f188aaacc92e5f9d068b6a9fe63205e060010
akvc <activeru@gmail.com>
2022-10-16 16:13:43 +0300

    Final commit

--- lesson5modules/victory.py
+++ lesson5modules/victory.py
@@ -3,11 +3,12 @@
 """
 # 1. Импорт конкретных функций из модуля
 # from <название модуля> import нужные функций через ,
-from famous_persons import get_random_person, get_person_and_question
+from lesson5modules.famous_persons import get_random_person, get_person_and_question
 
-rounds = int(input('Сколько раз вы хотите играть?'))
+def vv():
+    rounds = int(input('Сколько раз вы хотите играть?'))
 
-for i in range(rounds):
-    get_person_and_question()
+    for i in range(rounds):
+        get_person_and_question()
 
-print('Пока!')
+    print('Пока!')
--- new.py
+++ new.py
@@ -1,5 +1,7 @@
 # import lesson5modules\main.py
 import os
+import shutil
+import platform
 import sys
 
 #sys.path.append('lesson5modules')
@@ -14,10 +16,20 @@ sys_files={'.git','.idea','lesson5modules','LICENSE','new.py','oo.py','other.py'
 val = 0
 print('__name__',__name__)
 
-def list_dir():
+def list_dir(typ='a'):
     for filename in os.listdir('.'):
         if (filename not in sys_files):
-            print(f'{filename}')
+            show_name=False
+            if (typ=='a'):
+                show_name=True
+            if (typ=='d'):
+                if os.path.isdir('./'+filename):
+                    show_name=True
+            if (typ=='f'):
+                if os.path.isfile('./'+filename):
+                    show_name=True
+            if (show_name==True):
+                print(f'{filename}')
 
 
 while val == 0:
@@ -76,13 +88,57 @@ x. выход.''')
         print('Список папок и файлов:')
         list_dir()
         file = input('Скопировать что?')
-        if (os.is)
-        new_name
+        if (os.path.exists(file)):
+            print('Сейчас как скопирую...')
+            new_name=input('Новое имя файла:')
+            err=False
+            if (os.path.isdir(file)):
+                try:
+                    shutil.copytree(file,new_name)
+                except:
+                    err=True
+            if (os.path.isfile(file)):
+                try:
+                    val=shutil.copy(file,new_name)
+                except:
+                    err=True
+            if (err==True):
+                print('Произошла ошибка')
+            else:
+                print('Скопировано')
+        else:
+            print('Имя файла неверное')
+
 
     if (val=='4'):
         list_dir()
+    if (val=='5'):
+        list_dir(typ='d')
+    if (val == '6'):
+        list_dir(typ='f')
+    if (val == '7'):
+        print(platform.system())
+        print(platform.version())
+        uname_res=platform.uname()
+        print(uname_res)
+    if (val == '8'):
+        print('Кошкин Александр')
+    if (val == '9'):
+        from lesson5modules.victory import vv
+        try:
+            vv()
+        except:
+            print('Что-то пошло не так...')
+    if (val == '10'):
+        from baccount import start_baccount
+        start_baccount()
+    if (val == '11'):
+        print('Текущий каталог:'+os.getcwd())
+        new_path=input('Новый текущий каталог')
+        os.chdir(new_path)
+        os.getcwd()
     if val != 'X' and val != 'x' and val != 'Ч' and val != 'ч':
         val = 0
     # fp()
     print(f'-----КОНЕЦ РАБОТЫ-----')
-print (fp())
+print ('Завершение работы с программой')
